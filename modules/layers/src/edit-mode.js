a4496a966f51df40c60e0a2b4ad642c930bbc693
Clay Anderson <supersonicclay@gmail.com>
2019-05-06 12:42:22 -0600

    SQUASH ME: add edit mode

--- modules/layers/src/edit-mode.js
+++ modules/layers/src/edit-mode.js
@@ -1,65 +1,12 @@
 // @flow
 
-// Represents an edit action, i.e. a suggestion to update the data based on user interaction events
-export type EditAction<TData> = {
-  updatedData: TData,
-  editType: string,
-  affectedIndexes: number[],
-  editContext: any
-};
-
-// Represents an object "picked" from the screen. This usually reflects an object under the cursor
-export type Pick = {
-  object: any,
-  index: number,
-  isGuide: boolean
-};
-
-// Represents a click event
-export type ClickEvent = {
-  picks: Pick[],
-  screenCoords: Position,
-  mapCoords: Position,
-  sourceEvent: any
-};
-
-// Represents a double-click event
-export type DoubleClickEvent = {
-  mapCoords: Position,
-  sourceEvent: any
-};
-
-// Represents an event that occurs when the pointer goes down and the cursor starts moving
-export type StartDraggingEvent = {
-  picks: Pick[],
-  screenCoords: Position,
-  mapCoords: Position,
-  pointerDownScreenCoords: Position,
-  pointerDownMapCoords: Position,
-  sourceEvent: any
-};
-
-// Represents an event that occurs after the pointer goes down, moves some, then the pointer goes back up
-export type StopDraggingEvent = {
-  picks: Pick[],
-  screenCoords: Position,
-  mapCoords: Position,
-  pointerDownScreenCoords: Position,
-  pointerDownMapCoords: Position,
-  sourceEvent: any
-};
-
-// Represents an event that occurs every time the pointer moves
-export type PointerMoveEvent = {
-  screenCoords: Position,
-  mapCoords: Position,
-  picks: Pick[],
-  isDragging: boolean,
-  pointerDownPicks: ?(Pick[]),
-  pointerDownScreenCoords: ?Position,
-  pointerDownMapCoords: ?Position,
-  sourceEvent: any
-};
+import type {
+  EditAction,
+  ClickEvent,
+  PointerMoveEvent,
+  StartDraggingEvent,
+  StopDraggingEvent
+} from './event-types.js';
 
 export type ModeState<TData, TGuides> = {
   // The data being edited, this can be an array or an object
